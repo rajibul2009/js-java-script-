8a9321f17f0ff6c154bd50edcf4bbe815a6a46ad
Md. Rajibul Islam <rajibul2009@gmail.com>
2024-09-02 08:59:49 +0000

    pre

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,14 @@
+const accountID = 121212
+let accountEmail = "rajibul@gmail.com"
+var accountPassword ="1212"
+accountCity = "Dhaka"
+let accountState;
+
+
+/*accountEmail = "alam@gmail.com"
+accountPassword = "333333"
+accountCity = "mirpur"*/
+
+console.log(accountID);
+
+console.table([accountID,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basic/02_dataType.js
@@ -0,0 +1,8 @@
+"use strict"; //treat all JS code as newer version
+
+//alert( 3 + 3)
+
+//alert (3 + 3) // we are using nodejs, not broser
+
+console.log(3+3); console.log ("Rajib")
+
