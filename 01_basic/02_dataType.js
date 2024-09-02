1c0bd23ad1e38cff48b668c319e2805f6c00a79f
Md. Rajibul Islam <rajibul2009@gmail.com>
2024-09-02 09:38:26 +0000

    fff

--- 01_basic/02_dataType.js
+++ 01_basic/02_dataType.js
@@ -4,5 +4,32 @@
 
 //alert (3 + 3) // we are using nodejs, not broser
 
-console.log(3+3); console.log ("Rajib")
+console.log(3+
+    
+    3) //code readability should be hig
+ console.log ("Rajib")
+
+ let name = "Rajib"
+ let age = 38
+ let isLoggedIn = false
+ let state = null
+ let contry ;
+
+ //number => 2 to power 53
+ //bigint 
+ //string => ""
+ // boolean => true/false
+ // null => standalone value
+ // undefined => note value defined 
+// symbol = unique
+
+// object
+
+console.log(typeof "Rajib")  //string
+
+console.log(typeof age) //number
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
+
+
 
